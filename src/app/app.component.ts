11fd1883b7b690f630e5456c056d846e021d34da
K3nnythe13ed <thegodlikez@web.de>
2017-06-01 20:19:10 +0200

    init html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer, ViewChild, NgZone } from '@angular/core';
+require('slick-carousel');
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  constructor(private el: ElementRef, private zone: NgZone) {
+  }
+
+ 
+ $carousel: JQuery | any;
+
+  initialized = false;
+
+  initCarousel() {
+    this.zone.runOutsideAngular(() => {
+      this.$carousel = (<any>$('#myCarousel')).slick({});
+    });
+    this.initialized = true;
+  }
+
+  slides = [];
+
+  addSlide(slide) {
+    !this.initialized && this.initCarousel();
+    this.slides.push(slide);
+    this.$carousel.slick('slickAdd', slide.el.nativeElement);
+  }
+
+  removeSlide(slide) {
+    const idx = this.slides.indexOf(slide);
+    this.$carousel.slick('slickRemove', idx);
+    this.slides = this.slides.filter(s => s != slide);
+  }
 }
+
+
+  
